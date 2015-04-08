9ce30f18df475bfcf8d610bd3cd53a12c3cc9c93
gmbeatink <gregmershon2@msnc.om>
2015-04-07 20:51:01 -0600

    cool

--- BlankApp/MainPage.xaml.cs
+++ BlankApp/MainPage.xaml.cs
@@ -1,16 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -32,6 +21,7 @@ namespace BlankApp
                 Uri targetUri = new Uri(url);
                 GMWV1.Navigate(targetUri);
                 //yes!
+                //yes again
             }
             catch (FormatException myE)
             {
